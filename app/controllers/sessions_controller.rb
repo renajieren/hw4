32c49e643e47a3594cb9aca171bf871740b234d7
Rena  R <rena.ren@kellogg.northwestern.edu>
2024-03-08 00:32:11 +0000

    v2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,14 +8,14 @@ class SessionsController < ApplicationController
     if @user != nil
       if BCrypt::Password.new(@user["password"]) == params["password"]
       session["user_id"] = @user["id"]
-      flash["notice"] = "Welcome"
+      flash["notice"] = "Hello!"
       redirect_to "/places"    
       else
-      flash["notice"] = "Your email or password might be wrong"
+      flash["notice"] = "Nope. Forget your Email or password?"
       redirect_to "/login"
       end
     else
-      flash["notice"] = "Your email or password might be wrong"
+      flash["notice"] = "Nope. Forget your Email or password?"
       redirect_to "/login"
     end
 
